4c2340fa72137ae4a090312d8c05f114362286db
Dharmendra <dharmendrpanditx@gmail.com>
2026-07-02 17:11:38 +0545

    The blog section is compeleted and functioning well

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+// second-section javascript which is blog section 
+
+function openBlog() {
+    document.getElementById('placeholder-text').style.display = 'none';
+    document.getElementById('blog-frame').style.display = 'block';
+}
