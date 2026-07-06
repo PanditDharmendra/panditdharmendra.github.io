55c0de351deb937281b13fdc01f0fa6f60092f04
Dharmendra <dharmendrpanditx@gmail.com>
2026-07-06 12:10:27 +0545

    Written some java script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 
+alert("Welcome to Dharmendra Pandit's website!");
+console.log("code is running");
+document.title = "Dharmendra Pandit"
+
 // second-section javascript which is blog section 
 
 function openBlog() {
