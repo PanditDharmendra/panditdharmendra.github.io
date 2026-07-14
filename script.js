fdd1595107255d6d1f8e51c505b584be68f34671
Dharmendra <dharmendrpanditx@gmail.com>
2026-07-14 21:17:15 +0545

    Navigation bar responsivness is fixed now

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-
-
-console.log("code is running");
-document.title = "Dharmendra Pandit"
-
 // second-section javascript which is blog section 
-
 function openBlog() {
     document.getElementById('placeholder-text').style.display = 'none';
     document.getElementById('blog-frame').style.display = 'block';
